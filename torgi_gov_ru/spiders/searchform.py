ebe9ac821f37405827c58388dfd67b1e018526f2
Dmitry Sibiryakov <sibiriakoff2006@yandex.ru>
2024-05-16 08:01:49 +0300

    settings,

--- torgi_gov_ru/spiders/searchform.py
+++ torgi_gov_ru/spiders/searchform.py
@@ -1,5 +1,6 @@
 import scrapy
 from scrapy.http import TextResponse, Request
+import json
 # import 
 
 
@@ -9,6 +10,6 @@ class SearchformSpider(scrapy.Spider):
     start_urls = ["https://torgi.gov.ru/new/api/public/lotcards/search?dynSubjRF=12&lotStatus=PUBLISHED,APPLICATIONS_SUBMISSION,DETERMINING_WINNER&text=Жилой дом&byFirstVersion=true&withFacets=true&size=10&sort=firstVersionPublicationDate,desc"]
 
     def parse(self, response: TextResponse):
-        with open('response.txt', 'w', encoding='utf-8') as f:
-            f.write(response.text,)
+        with open('response.json', 'w', encoding='utf-8') as f:
+            f.write(json.dumps(response.json(), ensure_ascii=False, indent=4))
         # pass
